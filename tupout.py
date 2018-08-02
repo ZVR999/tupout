d43cb4bd7321f0c4ffeea27ec4d1c38d48fc6000
Zachkery Rodriguez <39169752+ZVR999@users.noreply.github.com>
2018-08-02 16:24:28 -0500

    Unable to output key/values in seperate tuples

--- tupout.py
+++ tupout.py
@@ -5,6 +5,9 @@ my_dict = {
   "Jay": "(777) 777-7777"
 }
 #function output
-[("Speros", "(555) 555-5555"), ("Michael", "(999) 999-9999"), ("Jay", "(777) 777-7777")]
+#[("Speros", "(555) 555-5555"), ("Michael", "(999) 999-9999"), ("Jay", "(777) 777-7777")]
 
+def tupOut(dic):
+    print [dic][0]
+tupOut(my_dict)
 # python tupout.py
